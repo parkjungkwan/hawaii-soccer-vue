79fc63a6581b5a2456f8ccedcc249b9c60d07db8
pakjkwan <pakjkwan@gmail.com>
2020-05-14 18:00:15 +0900

    18:00

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <layout>
+            <template #header="header">
+                <h1>{{header.title}}</h1>
+                <h3>로그인</h3>
+                <h3>회원가입</h3>
+            </template>
+        </layout>
+    </div>
+</template>
+
+<script>
+import Layout from "@/components/common/Layout.vue"
+export default {
+    name: "Home",
+    components : {Layout},
+    data : ()=>{
+        return {
+
+        }
+    }
+}
+</script>
---
+++ src/components/common/Layout.vue
@@ -0,0 +1,90 @@
+<template>
+    <div id="wrapper">
+        <header>
+            <slot name="header" v-bind:title="header"></slot>
+        </header>
+        <aside id="sidebar">
+
+        </aside>
+        <section id="content">
+
+        </section>
+        <footer>
+
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Layout",
+    data: ()=>{
+        return {
+            header : "헤더"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    /* 전체 구조 */
+    #wrapper {
+        padding: 5px;
+        width: 960px;
+        margin: 20px auto;
+    }
+    header {
+        height: 100px;
+        padding: 0 15px;
+    }
+    #content {
+        width: 696px;
+        float: left;
+        padding: 5px 15px;
+        min-height: 300px;
+    }
+    #sidebar {
+        width: 200px;
+        padding: 5px 15px;
+        float: left;
+    }
+    footer {
+        clear: both;
+        padding: 0 15px;
+    }
+    /* 가로폭 980보다 작을 때 사용할 @media query */
+    @media screen and (max-width: 980px) {
+        #pagewrap {
+            width: 94%;
+        }
+        #content {
+            clear: both;
+            padding: 1% 4%;
+            width: auto;
+            float: none;
+        }
+        #sidebar {
+            clear: both;
+            padding: 1% 4%;
+            width: auto;
+            float: none;
+        }
+        header, footer {
+            padding: 1% 4%;
+        }
+    }
+    /* 공통 */
+    #content {
+        background: #f8f8f8;
+    }
+    #sidebar {
+        background: #f0efef;
+    }
+    header, #content, #middle, #sidebar {
+        margin-bottom: 5px;
+    }
+    #pagewrap, header, #content, #middle, #sidebar, footer {
+        border: solid 1px #ccc;
+    }
+
+</style>
